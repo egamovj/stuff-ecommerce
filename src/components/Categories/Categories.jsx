917aa1b843b53ef2719407a26a6a3ce9ce2e28d7
egamovj <egamovj90@gmail.com>
2024-04-15 11:40:21 +0500

    categories section

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,5 +1,28 @@
-const Categories = () => {
-  return <div>Categories</div>;
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+
+import { Link } from "react-router-dom";
+import styles from "../../styles/Categories.module.css";
+
+const Categories = ({ title, style = {}, products = [], amount }) => {
+  const list = products.filter((_, i) => i < amount);
+
+  return (
+    <section className={styles.section}>
+      <h2>{title}</h2>
+      <div className={styles.list}>
+        {list.map(({ id, name, image }) => (
+          <Link to={`/categories/${id}`} key={id} className={styles.item}>
+            <div
+              className={styles.image}
+              style={{ backgroundImage: `url(${image})` }}
+            />
+            <h3 className={styles.title}>{name}</h3>
+          </Link>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Categories;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,16 @@
 import { useSelector } from "react-redux";
 import Poster from "../Poster/Poster";
 import Products from "../Products/Products";
+import Categories from "../Categories/Categories";
 
 const Home = () => {
-  const { list } = useSelector(({ products }) => products);
+  const { products, categories } = useSelector((state) => state);
 
   return (
     <>
       <Poster />
-      <Products products={list} amount={5} title="Trending" />
+      <Products products={products.list} amount={5} title="Trending" />
+      <Categories products={categories.list} amount={5} title="Worth seeing" />
     </>
   );
 };
